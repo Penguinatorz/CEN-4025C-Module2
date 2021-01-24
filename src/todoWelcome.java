b265240b138c4e0ce7ccce4f19347159d7d5ad2c
Penguinatorz <70828847+Penguinatorz@users.noreply.github.com>
2021-01-24 05:48:07 -0500

    Add files via upload

---
+++ src/todoWelcome.java
@@ -0,0 +1,14 @@
+public class todoWelcome {
+
+	public todoWelcome() {
+		System.out.println("------- Simple to-do list -------");
+		System.out.println(
+				"\n Enter from the following choices\n"
+				+ "1. Add to-do item\n"
+				+ "2. Delete to-do item\n"
+				+ "3. List to-do item\n"
+				+ "4. Exit");
+		return;
+}
+
+}
